a29cd3a634075ef686017db13f796635278115ab
github cjl248 <github cjl248@cornell.edu>
2019-09-23 16:21:39 -0400

    Done.

--- sushi-saga-client/src/App.js
+++ sushi-saga-client/src/App.js
@@ -7,10 +7,38 @@ const API = "http://localhost:3000/sushis"
 
 class App extends Component {
 
+  state = {
+    sushis: [],
+    index: 0
+  }
+
+  updateIndex = () => {
+    this.setState({
+      index: this.state.index+4
+    })
+  }
+
+  componentDidMount(){
+    fetch("http://localhost:3000/sushis")
+      .then(resp => resp.json())
+      .then(this.sushisHandler)
+  }
+
+  sushisHandler = (data) => {
+    this.setState({
+      sushis: data
+    })
+  }
+
   render() {
+    console.log("index of APP component: ", this.state.index)
     return (
       <div className="app">
-        <SushiContainer  />
+        <SushiContainer
+          sushis={this.state.sushis}
+          index={this.state.index}
+          updateIndex={this.updateIndex}
+        />
         <Table />
       </div>
     );
